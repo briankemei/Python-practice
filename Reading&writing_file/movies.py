2edfec8cde4a0a984f514fa9e758df36fcb30a91
Kemei Brian <51536845+briankemei@users.noreply.github.com>
2024-09-11 18:49:11 +0000

    Renamed python file to movies and formed movie.txt file

---
+++ Reading&writing_file/movies.py
@@ -0,0 +1,7 @@
+# Program to read and write in a file
+
+f = open("movie.txt","w")
+f.write("What is your favourite movie? List them down: ")
+print( )
+
+f = ()
--- Reading&writing_file/sports.py
+++
@@ -1,7 +0,0 @@
-# Program to read and write in a file
-
-f = open("sports.txt","w")
-f.write("\nMy favourite sport is cross track.")
-print( )
-
-f = ()
